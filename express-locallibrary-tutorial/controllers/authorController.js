702f0cce2329caef7527b96fa3ce56af8a3fb6b7
Jon Smith <aardvarkjon94@yahoo.com>
2021-03-13 13:20:29 -0600

    Added controllers and started catalog routes

---
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -0,0 +1,37 @@
+var Author = require('../models/author');
+
+//Display list of authors
+exports.author_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author list');
+}
+
+//Display Author create form on GET
+exports.author_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author create GET');
+};
+
+//Handle Author CREATE on POST
+exports.author_create_post = fucntion(req, res) {
+    res.send('NOT IMPLEMENTED: Author create POST');
+}
+
+//Display Author Delete form on GET
+exports.author_delete_get = fucntion(req, res) {
+    res.send('NOT IMPLEMENTED: Author delete GET');
+};
+
+//Handle Author delete on POST
+
+exports.author_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Autor delete POST');
+};
+
+// Display Author update form on GET.
+exports.author_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author update GET');
+};
+
+// Handle Author update on POST.
+exports.author_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author update POST');
+};
---
+++ express-locallibrary-tutorial/routes/catalog.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router()
+
+//require controller modules
+
+var book_controller = require('../controllers/bookController');
+var author_controller = require('../controllers/authorController');
+var genre_controller = require('../controllers/genreController');
+var book_instance_controller = require('../controllers/bookinstanceController');
+
+/////// BOOK ROUTES ////////
+
+//Get catalog home
+router.get('/', book_controller.index);
+
+// GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
+router.get('/book/create', book_controller.book_create_get);
+
