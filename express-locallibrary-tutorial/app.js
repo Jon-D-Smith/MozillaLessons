e8f0ba71bebf0115b9385997554f1dd147c7fad8
Jon Smith <aardvarkjon94@yahoo.com>
2021-03-14 14:19:03 -0500

    Setup index template

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -13,7 +13,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'Enter Database URL here';
+var mongoDB = 'Enter database URL here';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
