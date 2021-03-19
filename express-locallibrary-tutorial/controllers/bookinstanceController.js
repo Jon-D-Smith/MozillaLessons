5f1478834c202d7350e3f37e73c918934f87b42c
Jon Smith <aardvarkjon94@yahoo.com>
2021-03-19 15:11:33 -0500

    Created bookInstance Post controller

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -42,9 +42,42 @@ exports.bookinstance_create_get = function(req, res, next) {
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+exports.bookinstance_create_post = [
+
+    //Validate and sanatise fields
+    body('book', 'Book must be specified').trim().isLength({min : 1}).escape(),
+    body('imprint', 'Imprint must be specified').trim().isLength({ min: 1 }).escape(),
+    body('status').escape(),
+    body('due_back', 'Invalid date').optional({ checkFalsy: true }).isISO8601().toDate(),
+
+
+    (req, res, next) => {
+        const errors = ValidationResult(req);
+
+        var bookInstance = new BookInstance({
+            book: req.body.book,
+            imprint: req.body.imprint,
+            status: req.body.status,
+            due_back : req.body.due_back
+        });
+
+        if(!errors.isEmpty()) {
+            Book.find({}, 'title')
+            .exec(function(err, books) {
+                if(err) {return next(err);}
+
+                res.render('bookinstance_form', {title: 'Create BookInstance', book_list: books, selected_book:bookinstance.book._id, errors: errors.array(), bookinstance: bookinstance });
+
+            });
+            return;
+        }
+        else {
+            bookinstance.save(function(err){
+                res.redirect(bookinstance.url);
+            })
+        }
+    }
+];
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function(req, res) {
